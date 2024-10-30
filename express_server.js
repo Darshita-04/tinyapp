209c3028662fc9d8aaf7d45a736d68b0276e994a
Darshita Patel <pateldarshita1994@gmail.com>
2024-10-30 10:20:39 -0400

    login implemented using cookies

--- express_server.js
+++ express_server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 
 const urlDatabase = {
@@ -19,20 +20,21 @@ app.get("/", (req, res) => {
 // shows list of URL combinations
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 // redirects to new URL form page
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 // shows individual URL combination based on id
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
